7c0f320d220bcd2611186890c47f46a999042e6c
bogdancojo <bogdan.cojocaru97@icloud.com>
2021-05-22 16:06:52 +0300

    added authorize to sendemail

--- SoftbinatorHealthcare/SoftbinatorHealthcare/Controllers/SendEmailController.cs
+++ SoftbinatorHealthcare/SoftbinatorHealthcare/Controllers/SendEmailController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SoftbinatorHealthcare.DTO;
 using SoftbinatorHealthcare.Email;
 using SoftbinatorHealthcare.Models;
 using System;
@@ -14,17 +17,20 @@ namespace SoftbinatorHealthcare.Controllers
     public class SendEmailController : ControllerBase
     {
         private readonly IEmailSender emailSender;
-
-        public SendEmailController(IEmailSender _emailSender)
+        private readonly IMapper mapper;
+        public SendEmailController(IEmailSender _emailSender, IMapper _mapper)
         {
             emailSender = _emailSender;
+            mapper = _mapper;
         }
 
         public int FromBody { get; private set; }
 
+        [Authorize]
         [HttpGet]
-        public IActionResult Get([FromBody] Treatment treatment)
+        public IActionResult Get([FromBody] TreatmentDto treatmentDto)
         {
+            var treatment = mapper.Map<Treatment>(treatmentDto);
             var message = new Message(new string[] { "bogdan.cojocaru97@icloud.com" }, treatment);
             emailSender.SendEmail(message);
             return NoContent();
